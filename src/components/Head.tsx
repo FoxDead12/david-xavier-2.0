c92471dc8a240bb3b72454b78dab5792a765662d
David Xavier <davidxavier7512@gmail.com>
2023-01-18 03:32:41 +0000

    Change SEO Parts

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -11,6 +11,9 @@ export function HeadPage ({title, description}: any) {
             <meta name="description" content={description} />
             <meta name="robots" content="index, archive"/>
             <meta name="viewport" content="width=device-width" initial-scale="1" />
+
+            <link rel="alternate" hrefLang="en" href="https://davidxavier.me/en" />
+            <link rel="alternate" hrefLang="pt" href="https://davidxavier.me/pt" />
         </Head>
         </>
     )
