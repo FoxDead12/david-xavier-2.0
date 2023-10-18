99de7350693b8acabdc4c591aeb1cd1b70a495b5
David Xavier <davidxavier7512@gmail.com>
2023-10-19 00:47:36 +0100

    Add cloudware to experiences

--- src/components/Experiences.tsx
+++ src/components/Experiences.tsx
@@ -57,6 +57,15 @@ const experiencesArrayPT = [
         actual: "Atualmente",
         dataInicio: new Date("March 1, 2022"),
         dataFim: new Date("July 1, 2022")
+    },
+    {
+        left: false,
+        img: 'cloudware.jpeg',
+        cargo : 'Programador',
+        empresa: 'Cegid Cloudware',
+        localizacao: 'Porto',
+        actual: "Atualmente",
+        dataInicio: new Date("February 1, 2023"),
     }
 ];
 
@@ -111,11 +120,21 @@ const experiencesArrayEN = [
         actual: "Current",
         dataInicio: new Date("March 1, 2022"),
         dataFim: new Date("July 1, 2022")
+    },
+    ,
+    {
+        left: false,
+        img: 'cloudware.jpeg',
+        cargo : 'Developer',
+        empresa: 'Cegid Cloudware',
+        localizacao: 'Porto',
+        actual: "Atualmente",
+        dataInicio: new Date("February 1, 2023"),
     }
 ];
 
 export default function Experiences ({t, locale}: Props) {
-    
+
     const LoadExperiences = () => {
 
         if(locale === "pt") {
@@ -145,7 +164,7 @@ export default function Experiences ({t, locale}: Props) {
             <div className='relative w-[90%] m-auto 3xl:w-[70%] flex flex-col justify-between p-4 z-10'>
                 <h1 className='text-white text-lg italic'>{"<"}{t("experiences.section")}{">"}</h1>
                 <div className='relative w-[90%] m-auto 3xl:w-[90%] flex flex-col items-center justify-center my-24'>
-                    
+
                     <div className='relative w-full flex flex-col items-center justify-center py-4'>
 
                         <div className='absolute -right-8 md:right-auto  h-full w-4 bg-red-400 rounded-full'></div>
@@ -200,7 +219,7 @@ const Experience = (props: IExperience) => {
 
     return (
         <motion.div
-        
+
         initial={"initial"}
         whileInView={"animate"}
         variants={props.left === true? left: right}
@@ -208,28 +227,28 @@ const Experience = (props: IExperience) => {
         className={`relative flex-none w-full md:w-[350px] max-w-[350px]  bg-blue-800 opacity-100 md:opacity-70 hover:opacity-100 my-4 md:m-0 md:mx-4 p-4 rounded-md shadow-md  ${props.left === true ? 'md:mr-[450px]' : 'md:ml-[450px] md:mr-4'}`}>
 
             {/* Seta */}
-            <div className={`absolute invisible md:visible m-auto ${props.left === true ? '-right-2' : '-left-2' } top-0 bottom-0 w-4 h-4 rotate-45 bg-blue-800 `}></div> 
-            <div className={`absolute visible md:invisible m-auto -right-2 top-0 bottom-0 w-4 h-4 rotate-45 bg-blue-800 `}></div> 
+            <div className={`absolute invisible md:visible m-auto ${props.left === true ? '-right-2' : '-left-2' } top-0 bottom-0 w-4 h-4 rotate-45 bg-blue-800 `}></div>
+            <div className={`absolute visible md:invisible m-auto -right-2 top-0 bottom-0 w-4 h-4 rotate-45 bg-blue-800 `}></div>
 
 
 
             {/* Bola */}
-            <div className={`absolute invisible md:visible m-auto ${props.left === true ? '-right-[50px]' : '-left-[50px]' } top-0 bottom-0 w-4 h-4 rounded-full bg-white border-4 border-gray-900 `}></div> 
-            <div className={`absolute visible md:invisible m-auto -right-8 top-0 bottom-0 w-4 h-4 rounded-full bg-white border-4 border-gray-900 `}></div> 
+            <div className={`absolute invisible md:visible m-auto ${props.left === true ? '-right-[50px]' : '-left-[50px]' } top-0 bottom-0 w-4 h-4 rounded-full bg-white border-4 border-gray-900 `}></div>
+            <div className={`absolute visible md:invisible m-auto -right-8 top-0 bottom-0 w-4 h-4 rounded-full bg-white border-4 border-gray-900 `}></div>
 
             {/* Year */}
-            <div className={`absolute invisible md:visible m-auto flex items-center -right-[60px] -top-20 bottom-0  font-bold text-blue-900 rotate-0 ${props.left === true ? '-right-[60px]' : '-left-[60px]'}`}>
+            <div className={`absolute invisible md:visible m-auto flex items-center -right-[60px] -top-20 bottom-0  font-bold text-blue-900 rotate-0 ${props.left === true ? '-right-[3.90rem]' : '-left-[3.90rem]'}`}>
                 <h5 className='rotate-90 '>{props.dataInicio.getFullYear()}</h5>
             </div>
 
-            <div className={`absolute visible md:invisible m-auto flex items-center -right-[2.6rem] -top-20 bottom-0  font-bold text-blue-900 rotate-0 `}>
+            <div className={`absolute visible md:invisible m-auto flex items-center -right-[2.80rem] -top-20 bottom-0  font-bold text-blue-900 rotate-0 `}>
                 <h5 className='rotate-90 '>{props.dataInicio.getFullYear()}</h5>
             </div>
 
             <div className='flex items-center justify-center'>
                 <img src={"/img/logos/" + props.img} alt={props.empresa} className={"w-[100px] h-[100px] bg-white rounded-full object-contain shadow-md"} />
             </div>
-            
+
             <div className='flex flex-col items-start mt-8 mb-4'>
                 <h1 className='text-white text-base md:text-xl tracking-wider'>{props.cargo}</h1>
                 <h3 className='text-white text-sm md:text-base my-3'>{props.empresa}, {props.localizacao}</h3>
@@ -241,7 +260,7 @@ const Experience = (props: IExperience) => {
 
                 }
             </div>
-            
+
 
 
         </motion.div>
